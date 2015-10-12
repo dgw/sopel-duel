fc74278e56c99cef5577c1142baa701f345bf0d9
dgw <dgw@technobabbl.es>
2015-10-12 03:08:37 -0500

    Simpler handling of empty target argument
    See #3. Catching an exception is fancy and all, but it needn't be that complex.

--- duel.py
+++ duel.py
@@ -19,10 +19,7 @@ def duel(bot, trigger):
     if time_since < TIMEOUT:
         bot.notice("You must wait %d seconds until your next duel." % (TIMEOUT - time_since), trigger.nick)
         return module.NOLIMIT
-    try:
-        target = tools.Identifier(trigger.group(3))
-    except AttributeError:
-        target = None
+    target = tools.Identifier(trigger.group(3) or '')
     if not target:
         bot.reply("Who did you want to duel?")
         return module.NOLIMIT
